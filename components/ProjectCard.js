30f8baf1abd6d540e36c331a275b669d71916baf
sarashareef <88903058+sarashareef@users.noreply.github.com>
2023-08-14 22:16:32 -0400

    Update ProjectCard.js

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,13 +1,18 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, description2, description3 }) => {
   return (
-    <Col size={12} sm={6} md={4}>
+    <Col size={12} sm={6} md={6}>
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        <img 
+        src={imgUrl} 
+        />
         <div className="proj-txtx">
           <h4>{title}</h4>
-          <span>{description}</span>
+          <span>{description}</span><br>
+          </br>
+          <a href = "https://github.com/sarashareef/WeatherWiz-Pro/blob/main/weather.py"><span>{description2}</span></a>
+          <a href = "https://github.com/sarashareef/TaxMate"> <span>{description3}</span></a>
         </div>
       </div>
     </Col>
